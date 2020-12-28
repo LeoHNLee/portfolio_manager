ad14e0c2cd3cf107e72af6f6e6b83e82e290c6d1
Leohn <leohnlee@gmail.com>
2020-12-29 00:25:55 +0900

    [dev/manip] calc current

--- pm/calculator/current.py
+++ pm/calculator/current.py
@@ -3,14 +3,18 @@ import numpy as np
 
 
 def current_amt(name:str, tmp_df:pd.DataFrame):
+    if name in ('KRW', 'USD'):
+        return 1
     ret = tmp_df[tmp_df['종목번호']==name]['주문가능']
     if ret.shape[0] == 0:
         return 0
     return ret.values[0]
 
 
-def current_val(name:str, tmp_df:pd.DataFrame):
-    ret = tmp_df[tmp_df['종목번호']==name]['현재가']
+def current_val(row, tmp_df:pd.DataFrame):
+    if row['name'] in ('KRW', 'USD'):
+        return row['current_val']
+    ret = tmp_df[tmp_df['종목번호']==row['name']]['현재가']
     if ret.shape[0] == 0:
         return 0
     return ret.values[0]
@@ -25,19 +29,27 @@ def current_total(row):
 def virtual_total(row):
     if np.isnan(row['virtual_amt']):
         return 0
-    return row['virtual_amt'] * row['current_val']
+
+    if row['position'] == 'out':
+        return row['virtual_amt'] * row['virtual_val']
+    elif row['position'] == 'sell':
+        return row['virtual_amt'] * row['current_val']
+    elif row['position'] == 'buy':
+        return -1 * row['virtual_amt'] * row['current_val']
+    else:
+        return 0
 
 
-def calc_row(origin_df:pd.DataFrame, tmp_df:pd.DataFrame):
+def calc_current(origin_df:pd.DataFrame, tmp_df:pd.DataFrame, usd:int):
+    # input usd
     origin_df['current_amt'] = origin_df['name'].apply(lambda x: current_amt(x, tmp_df))
-    origin_df['current_val'] = origin_df['name'].apply(lambda x: current_val(x, tmp_df))
+    origin_df['current_val'] = origin_df.apply(lambda x: current_val(x, tmp_df))
     origin_df['current_total'] = origin_df.apply(current_total, axis=1)
     origin_df['virtual_total'] = origin_df.apply(virtual_total, axis=1)
+    origin_df['pivot_val'] = origin_df['pivot_rate'] * origin_df['current_val']
+
+    total = origin['current_total'].sum()
+    origin_df['target_total'] = origin_df['target_rate'] * total
+    origin_df['target_diff'] = origin_df['target_total'] - origin_df['current_total']
+    origin_df['virtual_diff'] = origin_df['virtual_total'] + origin_df['target_diff']
     return origin_df
-
-
-def calc_total(origin_df:pd.DataFrame):
-    # calc target_val
-    # calc diff_val
-    # calc trans_val
-    pass
