70e590c790918ec477462ac049d1ddb9c68a21d7
Leohn <leohnlee@gmail.com>
2020-12-30 21:01:12 +0900

    [dev/manip] total acnt w/ sh indi api

--- pm/control/manipulator/__init__.py
+++ pm/control/manipulator/__init__.py
@@ -12,6 +12,7 @@ from pm.control.casting import dt2str
 class Manipulator(Calculator):
     rt = ui.PaneControl(searchDepth=5, ClassName='GXWND', AutomationId='3779')
     set_krw = ui.ButtonControl(searchDepth=5, Name='원화기준')
+    rt_krw = ui.PaneControl(searchDepth=4, ClassName='GXWND', AutomationId='3837')
 
 
     @staticmethod
@@ -25,26 +26,17 @@ class Manipulator(Calculator):
         path:str=cfg.PATH_DATA,
         fn:str='origin.csv',
         backup:bool=True
-    ) -> Manipulator:
+    ):
         manip = Manipulator.read_csv(path, encoding='utf-8')
         if backup:
-            manip.to_csv(f'{path}backup/{dt2str(dt.now())}.csv')
+            manip.backup()
         return manip
-    
-    
-    @staticmethod
-    def get_stock_krw():
-        pass
 
 
-    @staticmethod
-    def get_usd():
-        pass
-
-
-    @staticmethod
-    def get_krw():
-        pass
+    def backup(self, backup_path:str=None):
+        if backup_path is None:
+            backup_path = f'{cfg.PATH_DATA}backup/{dt2str(dt.now())}.csv'
+        self.to_csv(backup_path)
 
 
     def manipulate(self, start_time:dt=None, end_time:dt=None):
@@ -56,17 +48,29 @@ class Manipulator(Calculator):
         while dt.now() < start_time:
             time.sleep(10)
 
-        self.get_stock_krw()
+        FLOW_KRW = self.get_flow_krw(Manipulator.rt_krw)
         while dt.now() < end_time:
             FLOW = self.get_flow(Manipulator.rt, Manipulator.set_krw)
             USD = 0
             KRW = 0
             self.calculate(FLOW)
             self['virtual_amt'] -= self.apply(self.bid_ask, axis=1)
+        self.backup()
 
 
-    def calc_stock_krw(self):
-        pass
+    @staticmethod
+    def get_flow_krw(
+        rt, 
+        path:str=cfg.PATH_DATA, 
+        fn:str='krw.csv'
+    ):
+        file_path = path+fn
+        rt.SetFocus()
+        rt.RightClick()
+        ui.MenuItemControl(searchDepth=3, Name='엑셀로 내보내기').Click()
+        ui.MenuItemControl(searchDepth=4, Name='CSV').Click()
+        ui.EditControl(searchDepth=6, Name='파일 이름(N):').SendKeys(file_path+'{Enter}')
+        return Manipulator.read_csv(file_path, encoding='cp949')
 
 
     @staticmethod
@@ -75,15 +79,14 @@ class Manipulator(Calculator):
         set_krw, 
         path:str=cfg.PATH_DATA, 
         fn:str='tmp.csv'
-    ) -> Manipulator:
-        file_path = path+fn
+    ):
         rt.SetFocus()
         set_krw.Click()
-        rt.RightClick()
-        ui.MenuItemControl(searchDepth=3, Name='엑셀로 내보내기').Click()
-        ui.MenuItemControl(searchDepth=4, Name='CSV').Click()
-        ui.EditControl(searchDepth=6, Name='파일 이름(N):').SendKeys(file_path+'{Enter}')
-        return Manipulator.read_csv(file_path, encoding='cp949')
+        return Manipulator.get_flow_krw(rt, path, fn)
+
+
+    def calc_flow_krw(self, krw_df):
+        pass
 
 
     def bid_ask(self, row) -> int:
@@ -116,11 +119,13 @@ class Manipulator(Calculator):
     @staticmethod
     def bid(amt:int):
         price = Manipulator.bid_ask_price()
+        # usd 조정
 
 
     @staticmethod
     def ask(amt:int):
         price = Manipulator.bid_ask_price()
+        # usd 조정
 
 
     @staticmethod
